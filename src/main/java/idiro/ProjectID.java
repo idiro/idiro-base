950b52cd9eceda2b88ed7796f30d94142d79658c
Etienne Dumoulin <etienne.dumoulin@idiro.com>
2013-02-27 11:49:35 +0000

    Add Eclipse settings and the ProjectID is read from properties now

--- src/main/java/idiro/ProjectID.java
+++ src/main/java/idiro/ProjectID.java
@@ -1,5 +1,9 @@
 package idiro;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Keep a track of the version of the software.
  * The name and the version has to be parsed 
@@ -14,25 +18,35 @@ public class ProjectID {
 	 * Name of the program
 	 */
 	private String name = null;
-	
+
 	/**
 	 * Version of the program
 	 */
 	private String version = null;
-	
-	private static ProjectID instance = new ProjectID();
-	
-	private ProjectID(){}
 
-	
+	private static ProjectID instance = new ProjectID();
+
+	private ProjectID(){
+		InputStream is = getClass().getResourceAsStream( "/META-INF/application.properties" );
+		Properties prop = new Properties();
+		try {
+			prop.load(is);
+			name = prop.getProperty("artifactId");
+			version = prop.getProperty("version");
+		} catch (IOException e) {
+			System.out.println("IOException cannot load name and version: "+e.getMessage());
+		}
+	}
+
+
 	public static String get(){
 		return instance.name+"-"+instance.version;
 	}
-	
+
 	public static boolean isInit(){
 		return !instance.name.isEmpty() && !instance.version.isEmpty();
 	}
-	
+
 	/**
 	 * @return the name
 	 */
@@ -41,8 +55,11 @@ public class ProjectID {
 	}
 
 	/**
+	 * Now the project name have to be set from an application.properties
+	 * file, in the META-INF directory of the jar
 	 * @param name the name to set
 	 */
+	@Deprecated
 	public void setName(String name) {
 		if(instance.name == null)
 			this.name = name;
@@ -56,8 +73,11 @@ public class ProjectID {
 	}
 
 	/**
+	 * Now the project version have to be set from an application.properties
+	 * file, in the META-INF directory of the jar
 	 * @param version the version to set
 	 */
+	@Deprecated
 	public void setVersion(String version) {
 		if(instance.version == null)
 			this.version = version;
@@ -69,5 +89,5 @@ public class ProjectID {
 	public static ProjectID getInstance() {
 		return instance;
 	}
-	
+
 }

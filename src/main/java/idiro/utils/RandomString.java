62709bd0c45257693576c3729d84079e587f4b17
Etienne Dumoulin <etienne.dumoulin@idiro.com>
2013-03-07 17:35:03 +0000

    Create a class to generate random string

---
+++ src/main/java/idiro/utils/RandomString.java
@@ -0,0 +1,31 @@
+package idiro.utils;
+
+import java.util.Random;
+
+public class RandomString {
+
+	public static String getRandomName(int length){
+		return getRandomName(length, 
+				"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+	}
+	
+	public static String getRandomName(int length, String characters){
+		Random rnd = new Random();
+		char[] new_name = new char[length];
+		for (int i = 0; i < length; ++i){
+			new_name[i] =  characters.charAt(rnd.nextInt(characters.length()));
+		}
+		return new String(new_name);
+	}
+	
+	public static String getRandomNameStartByLetter(int length){
+		String characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+		Random rnd = new Random();
+		char[] new_name = new char[length];
+		new_name[0] =  characters.charAt(rnd.nextInt(characters.length()-10));
+		for (int i = 1; i < length; i++){
+			new_name[i] =  characters.charAt(rnd.nextInt(characters.length()));
+		}
+		return new String(new_name);
+	}
+}
